fb94c8b5dc0210dd65e7cd6b265cc7eb21b41c89
Mariusz <mpostol@cas.eu>
2012-07-04 07:47:37 +0000

    [AWT-3479] Add data model generation project - SharePoint project contains general purpose helper functions, SPMetalHelper application creates spmetal parameter file. Issue #: 3479

---
+++ SPMetalHelper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CAS.SharePoint.SPMetalParameters;
+using Microsoft.SharePoint;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CAS.AgileWorkloadTracker.SPMetalHelper
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        PRWeb _web = PRWeb.ImportDataModel(Properties.Settings.Default.URL);
+        using (Stream _str = new FileInfo(Properties.Settings.Default.FileName).Create())
+          PRWeb.ImportDocument(_str, _web);
+        string _endText = String.Format("Operation finisched, the file is located at: {0}", Environment.CurrentDirectory);
+        MessageBox.Show(_endText, "Operation succeeded", MessageBoxButtons.OK, MessageBoxIcon.Information);
+      }
+      catch (Exception ex)
+      {
+        string _errorText = String.Format("Operation failed with the error: {0}", ex.Message);
+        MessageBox.Show(_errorText, "Operation failed",  MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+    }
+  }
+}
