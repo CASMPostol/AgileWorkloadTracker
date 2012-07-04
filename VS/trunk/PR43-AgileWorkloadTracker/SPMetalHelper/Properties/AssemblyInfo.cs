cace29c32661cf84c7341ba1217159e071129958
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-07-04 07:47:37 +0000

    [AWT-3479] Add data model generation project - SharePoint project contains general purpose helper functions, SPMetalHelper application creates spmetal parameter file. Issue #: 3479

---
+++ VS/trunk/PR43-AgileWorkloadTracker/SPMetalHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CAS.AgileWorkloadTrackerSPMetalHelper")]
+[assembly: AssemblyDescription("Creates entities for sharepoint lists and content types.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("AgileWorkloadTracker")]
+[assembly: AssemblyCopyright("Copyright © CAS 2012")]
+[assembly: AssemblyTrademark("AgileWorkloadTracker")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a60db9d-3fe1-4e8b-a6cd-5a7186e30cef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0")]
---
+++ VS/trunk/PR43-AgileWorkloadTracker/SharePoint/SPMetalParameters/Scripts/CreateCs.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "This file creates all the c# classes for xnl schemas"
+Get-Location | Write-host
+$env:path += "; C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\x64\"
+$cpath = get-location
+Write-host Processing
+set-location ..\
+xsd.exe "SPMetalParameters.xsd" /N:CAS.SharePoint.SPMetalParameters /c  |write-host
+set-location $cpath
+Write-host Done...
