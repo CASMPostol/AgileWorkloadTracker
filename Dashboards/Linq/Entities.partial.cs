891f7e667e116021faed8161d7eebba07b18b3f1
Mariusz <mpostol@cas.eu>
2012-07-04 13:19:36 +0000

    [AWT-3484] Current user resources localization - added partial function. Issue #: 3484

---
+++ Dashboards/Linq/Entities.partial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace CAS.AgileWorkloadTracker.Linq
+{
+  internal partial class Resources
+  {
+    internal static Resources FindForUser(Entities edc, SPUser _user)
+    {
+      if (edc.Resources == null)
+        return null;
+      return edc.Resources.FirstOrDefault(idx => String.IsNullOrEmpty(idx.EmployeeADAccount.Tytuł) ? false : idx.EmployeeADAccount.Tytuł.Contains(_user.Name));
+    }
+  }
+}
