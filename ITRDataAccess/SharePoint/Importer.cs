bda8e4a3507de380fc373600e79985f550bce492
Mariusz <mpostol@cas.eu>
2012-07-13 13:17:36 +0000

    [AWT-3506] Import data from Godziny - prepared infrastructure to implement the functionality: get data and submit to SharePoint. Issue #: 3506

--- ITRDataAccess/SharePoint/Importer.cs
+++ ITRDataAccess/SharePoint/Importer.cs
@@ -11,49 +11,60 @@ using TaskType = CAS.AgileWorkloadTracker.Linq.Type;
 
 namespace CAS.ITRDataAccess.SharePoint
 {
-  public partial class Importer: Component
+  public partial class Importer : Component
   {
+    #region ctor
     public Importer()
     {
       InitializeComponent();
     }
-    public Importer( IContainer container )
+    public Importer(IContainer container)
     {
-      container.Add( this );
+      container.Add(this);
       InitializeComponent();
     }
-    internal void Import( Bugnet.DatabaseContentDataSet m_BugNETDataSet )
+    #endregion
+
+    #region public
+    internal void Import(Bugnet.DatabaseContentDataSet m_BugNETDataSet)
     {
-      using ( Entities _entt = new Entities() )
+      using (Entities _entt = new Entities())
       {
         Stage _newStage = new Stage() { Title = "Imported" };
-        _entt.Stage.InsertOnSubmit( _newStage );
-        Import( m_BugNETDataSet.Project, _newStage, _entt );
-        Console.WriteLine( "Project" );
-        Import( m_BugNETDataSet.Version, _entt );
-        Console.WriteLine( "Version" );
-        Import( m_BugNETDataSet.Status, _entt );
-        Console.WriteLine( "Status" );
-        Import( m_BugNETDataSet.Priority, _entt );
-        Console.WriteLine( "Priority" );
-        Import( m_BugNETDataSet.Resolution, _entt );
-        Console.WriteLine( "Resolution" );
-        Import( m_BugNETDataSet.Type, _entt );
-        Console.WriteLine( "Type" );
-        Import( m_BugNETDataSet.Bug, _entt );
-        Console.WriteLine( "Bug" );
-        Import( m_BugNETDataSet.BugComment, _entt );
-        Console.WriteLine( "Starting SubmitChanges" );
+        _entt.Stage.InsertOnSubmit(_newStage);
+        Import(m_BugNETDataSet.Project, _newStage, _entt);
+        Console.WriteLine("Project");
+        Import(m_BugNETDataSet.Version, _entt);
+        Console.WriteLine("Version");
+        Import(m_BugNETDataSet.Status, _entt);
+        Console.WriteLine("Status");
+        Import(m_BugNETDataSet.Priority, _entt);
+        Console.WriteLine("Priority");
+        Import(m_BugNETDataSet.Resolution, _entt);
+        Console.WriteLine("Resolution");
+        Import(m_BugNETDataSet.Type, _entt);
+        Console.WriteLine("Type");
+        Import(m_BugNETDataSet.Bug, _entt);
+        Console.WriteLine("Bug");
+        Import(m_BugNETDataSet.BugComment, _entt);
+        Console.WriteLine("Starting SubmitChanges");
         _entt.SubmitChanges();
       }
 
     }
-    private void Import( Bugnet.DatabaseContentDataSet.ProjectDataTable projectDataTable, Stage stage, Entities _entt )
+    internal void Import(TimeTracking.TimeTrackingDataSet m_timeTrackingDataSet)
     {
-      foreach ( var _row in projectDataTable )
+      throw new NotImplementedException();
+    }
+    #endregion
+
+    #region Bugnet import
+    private void Import(Bugnet.DatabaseContentDataSet.ProjectDataTable projectDataTable, Stage stage, Entities _entt)
+    {
+      foreach (var _row in projectDataTable)
       {
 
-        Projects _new = Create<Projects>( _entt.Projects, m_ProjectsDictionary, _row.Name, _row.ProjectID );
+        Projects _new = Create<Projects>(_entt.Projects, m_ProjectsDictionary, _row.Name, _row.ProjectID);
         _new.Active = _row.Active > 0;
         _new.Body = _row.IsDescriptionNull() ? String.Empty : _row.Description;
         _new.Currency = Currency.PLN;
@@ -65,127 +76,132 @@ namespace CAS.ITRDataAccess.SharePoint
         _new.ProjectNumber = _row.Code;
         _new.ProjectStartDate = _row.CreateDate;
         _new.ProjectType = ProjectType.ProjectInternal;
-        _new.ProjectWarrantyDate = _row.CreateDate + TimeSpan.FromDays( 364 );
+        _new.ProjectWarrantyDate = _row.CreateDate + TimeSpan.FromDays(364);
       }
     }
-    private void Import( Bugnet.DatabaseContentDataSet.VersionDataTable versionDataTable, Entities _entt )
+    private void Import(Bugnet.DatabaseContentDataSet.VersionDataTable versionDataTable, Entities _entt)
     {
-      foreach ( var _row in versionDataTable )
+      foreach (var _row in versionDataTable)
       {
-        Milestone _new = Create<Milestone>( _entt.Milestone, m_MilestoneDictionary, _row.Name, _row.VersionID );
-        if ( m_ProjectsDictionary.ContainsKey( _row.ProjectID ) )
-          _new.Milestone2StageTitle = m_ProjectsDictionary[ _row.ProjectID ].Project2StageTitle;
+        Milestone _new = Create<Milestone>(_entt.Milestone, m_MilestoneDictionary, _row.Name, _row.VersionID);
+        if (m_ProjectsDictionary.ContainsKey(_row.ProjectID))
+          _new.Milestone2StageTitle = m_ProjectsDictionary[_row.ProjectID].Project2StageTitle;
         //TODO error handling mechnism
         //TODOD [AWT-3502] Add lookup from Milestones to Project  http://itrserver/Bugs/BugDetail.aspx?bid=3502
       }
     }
-    private void Import( Bugnet.DatabaseContentDataSet.ResolutionDataTable resutionDataTable, Entities _entt )
+    private void Import(Bugnet.DatabaseContentDataSet.ResolutionDataTable resutionDataTable, Entities _entt)
     {
-      foreach ( var _row in resutionDataTable )
+      foreach (var _row in resutionDataTable)
       {
-        Resolution _new = Create<Resolution>( _entt.Resolution, m_ResolutionDictionary, _row.Name, _row.ResolutionID );
+        Resolution _new = Create<Resolution>(_entt.Resolution, m_ResolutionDictionary, _row.Name, _row.ResolutionID);
       }
     }
-    private void Import( Bugnet.DatabaseContentDataSet.StatusDataTable statusDataTable, Entities _entt )
+    private void Import(Bugnet.DatabaseContentDataSet.StatusDataTable statusDataTable, Entities _entt)
     {
-      foreach ( var _row in statusDataTable )
+      foreach (var _row in statusDataTable)
       {
-        Status _new = Create<Status>( _entt.Status, m_StatusDictionary, _row.Name, _row.StatusID );
+        Status _new = Create<Status>(_entt.Status, m_StatusDictionary, _row.Name, _row.StatusID);
       }
     }
-    private void Import( Bugnet.DatabaseContentDataSet.TypeDataTable typeDataTable, Entities _entt )
+    private void Import(Bugnet.DatabaseContentDataSet.TypeDataTable typeDataTable, Entities _entt)
     {
-      foreach ( var _row in typeDataTable )
+      foreach (var _row in typeDataTable)
       {
-        TaskType _new = Create<TaskType>( _entt.Type, m_TaskTypeDictionary, _row.Name, _row.TypeID );
+        TaskType _new = Create<TaskType>(_entt.Type, m_TaskTypeDictionary, _row.Name, _row.TypeID);
       }
     }
-    private void Import( Bugnet.DatabaseContentDataSet.PriorityDataTable priorityDataTable, Entities _entt )
+    private void Import(Bugnet.DatabaseContentDataSet.PriorityDataTable priorityDataTable, Entities _entt)
     {
-      foreach ( var _row in priorityDataTable )
+      foreach (var _row in priorityDataTable)
       {
-        Priority _new = Create<Priority>( _entt.Priority, m_PriorityDictionary, _row.Name, _row.PriorityID );
+        Priority _new = Create<Priority>(_entt.Priority, m_PriorityDictionary, _row.Name, _row.PriorityID);
       }
     }
-    private void Import( Bugnet.DatabaseContentDataSet.BugDataTable bugDataTable, Entities _entt )
+    private void Import(Bugnet.DatabaseContentDataSet.BugDataTable bugDataTable, Entities _entt)
     {
-      foreach ( var item in bugDataTable )
+      foreach (var item in bugDataTable)
       {
-        Tasks _newTasks = Create<Tasks>( _entt.Task, m_TasksDictionary, item.Description, item.BugID );
-        _newTasks.Task2MilestoneDefinedInTitle = GetOrAdd<Milestone>( _entt.Milestone, m_MilestoneDictionary, item.VersionID );
-        _newTasks.Task2MilestoneResolvedInTitle = GetOrAdd<Milestone>( _entt.Milestone, m_MilestoneDictionary, item.FixedInVersionId );
-        _newTasks.Task2ProjectTitle = GetOrAdd<Projects>( _entt.Projects, m_ProjectsDictionary, item.ProjectID );
-        if ( !item.IsAssignedToUserIdNull() )
-          _newTasks.Task2ResourcesTitle = GetOrAdd<Resources>( _entt.Resources, m_ResourcesDictionary, item.AssignedToUserId );
+        Tasks _newTasks = Create<Tasks>(_entt.Task, m_TasksDictionary, item.Description, item.BugID);
+        _newTasks.Task2MilestoneDefinedInTitle = GetOrAdd<Milestone>(_entt.Milestone, m_MilestoneDictionary, item.VersionID);
+        _newTasks.Task2MilestoneResolvedInTitle = GetOrAdd<Milestone>(_entt.Milestone, m_MilestoneDictionary, item.FixedInVersionId);
+        _newTasks.Task2ProjectTitle = GetOrAdd<Projects>(_entt.Projects, m_ProjectsDictionary, item.ProjectID);
+        if (!item.IsAssignedToUserIdNull())
+          _newTasks.Task2ResourcesTitle = GetOrAdd<Resources>(_entt.Resources, m_ResourcesDictionary, item.AssignedToUserId);
         else
           _newTasks.Task2ResourcesTitle = null;
-        _newTasks.Task2SPriorityTitle = GetOrAdd<Priority>( _entt.Priority, m_PriorityDictionary, item.PriorityID );
-        _newTasks.Task2SResolutionTitle = GetOrAdd<Resolution>( _entt.Resolution, m_ResolutionDictionary, item.ResolutionID );
-        _newTasks.Task2StatusTitle = GetOrAdd<Status>( _entt.Status, m_StatusDictionary, item.StatusID );
-        _newTasks.Task2TypeTitle = GetOrAdd<TaskType>( _entt.Type, m_TaskTypeDictionary, item.TypeID );
+        _newTasks.Task2SPriorityTitle = GetOrAdd<Priority>(_entt.Priority, m_PriorityDictionary, item.PriorityID);
+        _newTasks.Task2SResolutionTitle = GetOrAdd<Resolution>(_entt.Resolution, m_ResolutionDictionary, item.ResolutionID);
+        _newTasks.Task2StatusTitle = GetOrAdd<Status>(_entt.Status, m_StatusDictionary, item.StatusID);
+        _newTasks.Task2TypeTitle = GetOrAdd<TaskType>(_entt.Type, m_TaskTypeDictionary, item.TypeID);
       }
     }
-    private void Import( Bugnet.DatabaseContentDataSet.BugCommentDataTable bugCommentDataTable, Entities _entt )
+    private void Import(Bugnet.DatabaseContentDataSet.BugCommentDataTable bugCommentDataTable, Entities _entt)
     {
-      foreach ( var _row in bugCommentDataTable )
+      foreach (var _row in bugCommentDataTable)
       {
         // Create<TaskComments>( _entt.TaskComments, m_TaskCommentsDictionary, String.Empty, _row.BugCommentID );
         TaskComments _new = new TaskComments()
         {
           Body = _row.Comment,
-          TaskComments2TaskTitle = GetOrAdd<Tasks>( _entt.Task, m_TasksDictionary, _row.BugID )
+          TaskComments2TaskTitle = GetOrAdd<Tasks>(_entt.Task, m_TasksDictionary, _row.BugID)
         };
-        _entt.TaskComments.InsertOnSubmit( _new );
+        _entt.TaskComments.InsertOnSubmit(_new);
         //m_TaskCommentsDictionary.Add( _row.BugCommentID, _new );
       }
     }
+    #endregion
+    
+    #region TimeTracking import
+    #endregion
+
     #region data management
-    private type Create<type>( EntityList<type> list, Dictionary<int, type> _dictionary, string title, int _key )
-      where type: Element, new()
+    private type Create<type>(EntityList<type> list, Dictionary<int, type> _dictionary, string title, int _key)
+      where type : Element, new()
     {
       type _elmnt = new type();
-      if ( !String.IsNullOrEmpty( title ) )
+      if (!String.IsNullOrEmpty(title))
         _elmnt.Title = title;
-      if ( _dictionary.Keys.Contains( _key ) )
+      if (_dictionary.Keys.Contains(_key))
         _key = UniqueKey();
-      _dictionary.Add( _key, _elmnt );
-      list.InsertOnSubmit( _elmnt );
+      _dictionary.Add(_key, _elmnt);
+      list.InsertOnSubmit(_elmnt);
       return _elmnt;
     }
-    private type Create<type>( EntityList<type> list, Dictionary<Guid, type> _dictionary, string title, Guid _key )
-      where type: Element, new()
+    private type Create<type>(EntityList<type> list, Dictionary<Guid, type> _dictionary, string title, Guid _key)
+      where type : Element, new()
     {
       type _elmnt = new type();
-      if ( !String.IsNullOrEmpty( title ) )
+      if (!String.IsNullOrEmpty(title))
         _elmnt.Title = title;
-      if ( _dictionary.Keys.Contains( _key ) )
+      if (_dictionary.Keys.Contains(_key))
         _key = Guid.NewGuid();
-      _dictionary.Add( _key, _elmnt );
-      list.InsertOnSubmit( _elmnt );
+      _dictionary.Add(_key, _elmnt);
+      list.InsertOnSubmit(_elmnt);
       return _elmnt;
     }
-    private type GetOrAdd<type>( EntityList<type> _EDC, Dictionary<int, type> _dictionary, int? _key )
-      where type: Element, new()
+    private type GetOrAdd<type>(EntityList<type> _EDC, Dictionary<int, type> _dictionary, int? _key)
+      where type : Element, new()
     {
-      if ( !_key.HasValue )
+      if (!_key.HasValue)
         _key = UniqueKey();
-      else if ( _dictionary.ContainsKey( _key.Value ) )
-        return _dictionary[ _key.Value ];
-      return Create<type>( _EDC, _dictionary, EmptyKey( _key.Value ), _key.Value );
+      else if (_dictionary.ContainsKey(_key.Value))
+        return _dictionary[_key.Value];
+      return Create<type>(_EDC, _dictionary, EmptyKey(_key.Value), _key.Value);
     }
-    private type GetOrAdd<type>( EntityList<type> _EDC, Dictionary<Guid, type> _dictionary, Guid? _key )
-      where type: Element, new()
+    private type GetOrAdd<type>(EntityList<type> _EDC, Dictionary<Guid, type> _dictionary, Guid? _key)
+      where type : Element, new()
     {
-      if ( !_key.HasValue )
+      if (!_key.HasValue)
         _key = Guid.NewGuid();
-      else if ( _dictionary.ContainsKey( _key.Value ) )
-        return _dictionary[ _key.Value ];
-      return Create<type>( _EDC, _dictionary, EmptyKey( _key.Value ), _key.Value );
+      else if (_dictionary.ContainsKey(_key.Value))
+        return _dictionary[_key.Value];
+      return Create<type>(_EDC, _dictionary, EmptyKey(_key.Value), _key.Value);
     }
     private short m_EmptyKeyIdx = 0;
     private int UniqueKey() { return m_EmptyKeyIdx--; }
-    private static string EmptyKey( int key ) { return String.Format( CultureInfo.InvariantCulture, "EmptyKey{0}", key ); }
-    private static string EmptyKey( Guid key ) { return String.Format( CultureInfo.InvariantCulture, "EmptyKey{0}", key ); }
+    private static string EmptyKey(int key) { return String.Format(CultureInfo.InvariantCulture, "EmptyKey{0}", key); }
+    private static string EmptyKey(Guid key) { return String.Format(CultureInfo.InvariantCulture, "EmptyKey{0}", key); }
     #endregion
 
     #region Dictionaries
@@ -199,5 +215,6 @@ namespace CAS.ITRDataAccess.SharePoint
     private Dictionary<int, TaskComments> m_TaskCommentsDictionary = new Dictionary<int, TaskComments>();
     private Dictionary<Guid, Resources> m_ResourcesDictionary = new Dictionary<Guid, Resources>();
     #endregion
+
   }
 }
