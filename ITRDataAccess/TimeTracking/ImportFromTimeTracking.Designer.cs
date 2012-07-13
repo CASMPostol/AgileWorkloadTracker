af79a1d001e6d48cab6d67566b605c465e6bc778
Mariusz <mpostol@cas.eu>
2012-07-13 14:39:00 +0000

    [AWT-3506] Import data from Godziny - added all relevant files, read from database implemented and tested. Ready to be imported to SharePoint. Issue #: 3506

---
+++ ITRDataAccess/TimeTracking/ImportFromTimeTracking.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CAS.ITRDataAccess.TimeTracking
+{
+  partial class ImportFromTimeTracking
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.m_timeTrackingDataSet = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSet();
+      this.m_SharePointImporter = new CAS.ITRDataAccess.SharePoint.Importer(this.components);
+      this.m_godzinyTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.GODZINYTableAdapter();
+      this.m_kategorieTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.KATEGORIETableAdapter();
+      this.m_kontrahenciTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.KONTRAHENCITableAdapter();
+      this.m_planTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.PLANTableAdapter();
+      this.m_platnosciTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.PLATNOSCITableAdapter();
+      this.m_podkategorieTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.PODKATEGORIETableAdapter();
+      this.m_polisyTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.POLISYTableAdapter();
+      this.m_pracownicyTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.PRACOWNICYTableAdapter();
+      this.m_projektyTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.PROJEKTYTableAdapter();
+      this.m_rodzajpracyTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.RODZAJPRACYTableAdapter();
+      this.m_statusyTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.STATUSYTableAdapter();
+      this.m_umowyTableAdapter = new CAS.ITRDataAccess.TimeTracking.TimeTrackingDataSetTableAdapters.UMOWYTableAdapter();
+      ((System.ComponentModel.ISupportInitialize)(this.m_timeTrackingDataSet)).BeginInit();
+      // 
+      // m_timeTrackingDataSet
+      // 
+      this.m_timeTrackingDataSet.DataSetName = "TimeTrackingDataSet";
+      this.m_timeTrackingDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+      // 
+      // m_godzinyTableAdapter
+      // 
+      this.m_godzinyTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_kategorieTableAdapter
+      // 
+      this.m_kategorieTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_kontrahenciTableAdapter
+      // 
+      this.m_kontrahenciTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_planTableAdapter
+      // 
+      this.m_planTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_platnosciTableAdapter
+      // 
+      this.m_platnosciTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_podkategorieTableAdapter
+      // 
+      this.m_podkategorieTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_polisyTableAdapter
+      // 
+      this.m_polisyTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_pracownicyTableAdapter
+      // 
+      this.m_pracownicyTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_projektyTableAdapter
+      // 
+      this.m_projektyTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_rodzajpracyTableAdapter
+      // 
+      this.m_rodzajpracyTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_statusyTableAdapter
+      // 
+      this.m_statusyTableAdapter.ClearBeforeFill = true;
+      // 
+      // m_umowyTableAdapter
+      // 
+      this.m_umowyTableAdapter.ClearBeforeFill = true;
+      ((System.ComponentModel.ISupportInitialize)(this.m_timeTrackingDataSet)).EndInit();
+
+    }
+
+    #endregion
+
+    private TimeTrackingDataSet m_timeTrackingDataSet;
+    private SharePoint.Importer m_SharePointImporter;
+    private TimeTrackingDataSetTableAdapters.GODZINYTableAdapter m_godzinyTableAdapter;
+    private TimeTrackingDataSetTableAdapters.KATEGORIETableAdapter m_kategorieTableAdapter;
+    private TimeTrackingDataSetTableAdapters.KONTRAHENCITableAdapter m_kontrahenciTableAdapter;
+    private TimeTrackingDataSetTableAdapters.PLANTableAdapter m_planTableAdapter;
+    private TimeTrackingDataSetTableAdapters.PLATNOSCITableAdapter m_platnosciTableAdapter;
+    private TimeTrackingDataSetTableAdapters.PODKATEGORIETableAdapter m_podkategorieTableAdapter;
+    private TimeTrackingDataSetTableAdapters.POLISYTableAdapter m_polisyTableAdapter;
+    private TimeTrackingDataSetTableAdapters.PRACOWNICYTableAdapter m_pracownicyTableAdapter;
+    private TimeTrackingDataSetTableAdapters.PROJEKTYTableAdapter m_projektyTableAdapter;
+    private TimeTrackingDataSetTableAdapters.RODZAJPRACYTableAdapter m_rodzajpracyTableAdapter;
+    private TimeTrackingDataSetTableAdapters.STATUSYTableAdapter m_statusyTableAdapter;
+    private TimeTrackingDataSetTableAdapters.UMOWYTableAdapter m_umowyTableAdapter;
+  }
+}
