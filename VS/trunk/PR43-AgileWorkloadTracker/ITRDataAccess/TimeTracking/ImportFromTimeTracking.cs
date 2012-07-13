1233db05fbd38f2e1dba1a6d4060cef4e0d361a4
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-07-13 14:39:00 +0000

    [AWT-3506] Import data from Godziny - added all relevant files, read from database implemented and tested. Ready to be imported to SharePoint. Issue #: 3506

---
+++ VS/trunk/PR43-AgileWorkloadTracker/ITRDataAccess/TimeTracking/ImportFromTimeTracking.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace CAS.ITRDataAccess.TimeTracking
+{
+  public partial class ImportFromTimeTracking : Component
+  {
+    public ImportFromTimeTracking()
+    {
+      InitializeComponent();
+    }
+    public ImportFromTimeTracking(IContainer container)
+    {
+      if (container == null)
+        throw new ArgumentNullException("container cannot be null");
+      container.Add(this);
+      InitializeComponent();
+    }
+    internal void GetDataFromDatabase()
+    {
+      m_rodzajpracyTableAdapter.Fill(m_timeTrackingDataSet.RODZAJPRACY);
+      m_statusyTableAdapter.Fill(m_timeTrackingDataSet.STATUSY);
+      m_kategorieTableAdapter.Fill(m_timeTrackingDataSet.KATEGORIE);
+      m_podkategorieTableAdapter.Fill(m_timeTrackingDataSet.PODKATEGORIE);
+      m_pracownicyTableAdapter.Fill(m_timeTrackingDataSet.PRACOWNICY);
+      m_kontrahenciTableAdapter.Fill(m_timeTrackingDataSet.KONTRAHENCI);
+      m_umowyTableAdapter.Fill(m_timeTrackingDataSet.UMOWY);
+      m_platnosciTableAdapter.Fill(m_timeTrackingDataSet.PLATNOSCI);
+      m_polisyTableAdapter.Fill(m_timeTrackingDataSet.POLISY);
+      m_projektyTableAdapter.Fill(m_timeTrackingDataSet.PROJEKTY);
+      m_godzinyTableAdapter.Fill(m_timeTrackingDataSet.GODZINY);
+      m_planTableAdapter.Fill(m_timeTrackingDataSet.PLAN);
+      m_SharePointImporter.Import(m_timeTrackingDataSet);
+    }
+  }
+}
