2df95a635cd2007f9e5eb4b2b022a9cd68e81ce9
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-07-13 20:14:40 +0000

    [AWT-3504] Submitting TaskComment throws exception - elements are duplicated - added filtering mechanism for Title and body fields.
    Issue #: 3504

---
+++ VS/trunk/PR43-AgileWorkloadTracker/ITRDataAccess/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.ITRDataAccess
+{
+  public static class Extensions
+  {
+    public static string SPValidSubstring( this string _value )
+    {
+      string _normalizedString = Microsoft.SharePoint.Utilities.SPStringUtility.RemoveControlChars( _value );
+      int _gdl = _normalizedString.Length;
+      do
+      {
+        _normalizedString = _normalizedString.Replace( "  ", " " );
+        if ( _gdl == _normalizedString.Length )
+          break;
+        _gdl = _normalizedString.Length;
+      } while ( true );
+      int SPStringMAxLength = 250;
+      if ( _gdl >= SPStringMAxLength )
+        _normalizedString = _normalizedString.Remove( SPStringMAxLength );
+      return _normalizedString;
+    }
+  }
+}
