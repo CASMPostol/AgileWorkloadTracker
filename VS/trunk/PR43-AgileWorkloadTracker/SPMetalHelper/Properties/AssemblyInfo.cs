4cd32ae7e29be5adb1bd10ca7c5422b466d38acc
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-07-16 08:45:42 +0000

    [AWT-3514] Use common functionality from the SharePoint library - refactoring, unimportant changes.
    Issue #: 3514

--- VS/trunk/PR43-AgileWorkloadTracker/SPMetalHelper/Properties/AssemblyInfo.cs
+++ VS/trunk/PR43-AgileWorkloadTracker/SPMetalHelper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CAS.AgileWorkloadTrackerSPMetalHelper")]
-[assembly: AssemblyDescription("Creates entities for sharepoint lists and content types.")]
+[assembly: AssemblyDescription( "Contains common data model for all SharePoint solutions." )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
 [assembly: AssemblyProduct("AgileWorkloadTracker")]
