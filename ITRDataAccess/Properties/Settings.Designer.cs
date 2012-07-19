dca08ccf665e5c2a46638c248f00d4947242512b
Mariusz <mpostol@cas.eu>
2012-07-19 14:17:06 +0000

    [AWT-3506] Import data from Godziny - added additional try catch to allow finishing of the program in case of errors. Issue #: 3506

--- ITRDataAccess/Properties/Settings.Designer.cs
+++ ITRDataAccess/Properties/Settings.Designer.cs
@@ -35,7 +35,7 @@ namespace CAS.ITRDataAccess.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://casmp/sites/a07")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://cas_sp:11225/sites/awt")]
         public string RequestUrl {
             get {
                 return ((string)(this["RequestUrl"]));
