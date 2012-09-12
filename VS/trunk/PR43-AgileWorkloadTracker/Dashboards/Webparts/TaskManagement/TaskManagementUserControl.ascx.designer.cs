87dbeff9075f20a68afcd07a8468f6135525e78e
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-09-12 08:32:53 +0000

    [AWT-3689] Modifie TaskManagement WebPart - add label for project - modified. Issue #: 3689

--- VS/trunk/PR43-AgileWorkloadTracker/Dashboards/Webparts/TaskManagement/TaskManagementUserControl.ascx.designer.cs
+++ VS/trunk/PR43-AgileWorkloadTracker/Dashboards/Webparts/TaskManagement/TaskManagementUserControl.ascx.designer.cs
@@ -40,6 +40,24 @@ namespace CAS.AgileWorkloadTracker.Dashboards.Webparts.TaskManagement {
         protected global::System.Web.UI.WebControls.Table m_TableMain;
         
         /// <summary>
+        /// m_ProjectLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label m_ProjectLabel;
+        
+        /// <summary>
+        /// m_DisplaySelectedProject control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label m_DisplaySelectedProject;
+        
+        /// <summary>
         /// m_TaskLabel control.
         /// </summary>
         /// <remarks>
