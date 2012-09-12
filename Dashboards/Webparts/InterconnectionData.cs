436c36ee48c185c7b6e166c7d48a32c91008ae4c
Mariusz <mpostol@cas.eu>
2012-09-12 07:19:31 +0000

    [AWT-3535] Category must be added to the application. Issue #: 3535

---
+++ Dashboards/Webparts/InterconnectionData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CAS.AgileWorkloadTracker.Linq;
+
+namespace CAS.AgileWorkloadTracker.Dashboards.Webparts
+{
+  internal enum ConnectionSelector
+  {
+    ProjectInterconnection,
+    TaskInterconnection,
+  }
+  internal abstract class InterconnectionData<DerivedType>: CAS.SharePoint.Web.InterconnectionData<DerivedType>
+      where DerivedType: InterconnectionData<DerivedType>
+  {
+    internal string ID { get { return GetFieldValue( Element.IDColunmName ); } }
+    internal string Title { get { return GetFieldValue( Element.TitleColunmName ); } }
+  }
+  internal class ProjectInterconnectionData: InterconnectionData<ProjectInterconnectionData>
+  {
+    internal ProjectInterconnectionData()
+      : base()
+    { }
+  }
+  internal class TaskInterconnectionData: InterconnectionData<TaskInterconnectionData>
+  {
+    internal TaskInterconnectionData()
+      : base()
+    { }
+  }
+}
