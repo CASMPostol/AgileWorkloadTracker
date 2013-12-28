cab9ce2cd26662cfd2112ea7074392bcfb65c0f9
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-12-28 19:59:47 +0000

    Move requirement in progress to a new milestone - work in progress - refactoring added keywords.

--- VS/trunk/PR43-AgileWorkloadTracker/AgileWorkloadTracker/Workflows/CalculateProject/CalculateProject.cs
+++ VS/trunk/PR43-AgileWorkloadTracker/AgileWorkloadTracker/Workflows/CalculateProject/CalculateProject.cs
@@ -4,11 +4,11 @@ using System.ComponentModel.Design;
 //<summary>
 //  Title   : Calculate Project Sequential Workflow Activity
 //  System  : Microsoft Visual C# .NET 2012
-//  $LastChangedDate:$
-//  $Rev:$
-//  $LastChangedBy:$
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
 //  $URL:$
-//  $Id:$
+//  $Id$
 //
 //  Copyright (C) 2013, CAS LODZ POLAND.
 //  TEL: +48 (42) 686 25 47
