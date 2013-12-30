5009ae2ab8d78d4bce24de3856c69195d83e0c1d
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-12-30 12:44:47 +0000

    Move requirement in progress to a new milestone - work in progress - added Description to the MilestoneWrapper.

--- VS/trunk/PR43-AgileWorkloadTracker/ServerHelpers/SiteManagement/Linq/MilestoneWrapper.cs
+++ VS/trunk/PR43-AgileWorkloadTracker/ServerHelpers/SiteManagement/Linq/MilestoneWrapper.cs
@@ -1,11 +1,11 @@
 ﻿//<summary>
 //  Title   : class MilestoneWrapper 
 //  System  : Microsoft Visual C# .NET 2012
-//  $LastChangedDate:$
-//  $Rev:$
-//  $LastChangedBy:$
-//  $URL:$
-//  $Id:$
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
 //
 //  Copyright (C) 2013, CAS LODZ POLAND.
 //  TEL: +48 (42) 686 25 47
@@ -14,7 +14,9 @@
 //</summary>
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using CAS.AgileWorkloadTracker.SiteManagement;
 
 namespace CAS.AgileWorkloadTracker.SiteManagement.Linq
@@ -27,11 +29,34 @@ namespace CAS.AgileWorkloadTracker.SiteManagement.Linq
     {
       if (milestone == null)
         return;
-      b_ActiveTasks = Element.Tasks0.Where<DataModel.Linq.Tasks>(x => x.Active.GetValueOrDefault(false)).Count();
+      List<DataModel.Linq.Tasks> _activeTasksList = (from _tsx in Element.Tasks0
+                                                     let _rt = _tsx.Task2RequirementsTitle.Title
+                                                     where _tsx.Active.GetValueOrDefault(false)
+                                                     orderby _rt
+                                                     select _tsx).ToList();
+      b_ActiveTasks = _activeTasksList.Count;
+      StringBuilder _description = new StringBuilder();
+      double _hours = 0;
+      foreach (var _at in _activeTasksList)
+        _hours += _at.Workload.Sum(x => x.Hours.GetValueOrDefault(0));
+      _description.AppendFormat("{0}: Accepted Hours={1}, Estimated Hours={2}, Hours {3}", milestone.Title, milestone.AcceptedHours.GetValueOrDefault(0), milestone.EstimatedHours.GetValueOrDefault(0), _hours);
+      foreach (var _rqx in milestone.Requirements)
+      {
+        _description.AppendLine(_rqx.Title);
+      }
+      b_Description = _description.ToString();
     }
-
-    private int b_ActiveTasks;
-
+    public string Description
+    {
+      get
+      {
+        return b_Description;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_Description, "Description", this);
+      }
+    }
     public int ActiveTasks
     {
       get
@@ -51,6 +76,11 @@ namespace CAS.AgileWorkloadTracker.SiteManagement.Linq
         return base.ToString();
       return String.Format("{0} Active tasks {1}", Element.Title, ActiveTasks);
     }
+    #endregion
+
+    #region private
+    private string b_Description;
+    private int b_ActiveTasks;
     #endregion
 
   }
