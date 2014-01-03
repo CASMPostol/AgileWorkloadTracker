8f7cfbfb0eeaf0725e06f51cf8fd513fe93ceb3f
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-01-03 22:31:51 +0000

    Move requirement in progress to a new milestone - in testing - copy of tasks has status new.

---
+++ VS/trunk/PR43-AgileWorkloadTracker/AgileWorkloadTracker/DataModel/Linq/Status.cs
@@ -0,0 +1,39 @@
+﻿//<summary>
+//  Title   : partial class Status
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+
+namespace CAS.AgileWorkloadTracker.DataModel.Linq
+{
+  /// <summary>
+  /// Entity partial class Status
+  /// </summary>
+  public partial class Status
+  {
+    internal enum StatusValues : int
+    {
+      Confirmed = 1,
+      InProgress = 2,
+      ReOpened = 3,
+      Resolved = 4,
+      Closed = 5,
+      InWaiting = 6,
+      New = 7
+    }
+    internal static Status GetStatus(Entities edc, StatusValues value)
+    {
+      return Element.GetAtIndex<Status>(edc.Status, (int)value);
+    }
+  }
+}
