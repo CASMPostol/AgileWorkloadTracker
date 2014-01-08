ba80b409769254f8966aed09fa59b7f07ef80020
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-01-08 13:17:59 +0000

    Create tag, close this sprint - setup current version - added project for scripts.

---
+++ VS/trunk/PR43-AgileWorkloadTracker/Scripts/CreateTag.cmd
@@ -0,0 +1,22 @@
+
+rem//  $LastChangedDate: 2013-08-29 13:53:38 +0200 (Thu, 29 Aug 2013) $
+rem//  $Rev: 9645 $
+rem//  $LastChangedBy: mpostol $
+rem//  $URL: svn://svnserver.hq.cas.com.pl/VS/trunk/PR42-SmartFactory/Scripts/CreateTag.tt $
+rem//  $Id: CreateTag.tt 9645 2013-08-29 11:53:38Z mpostol $
+
+
+set branchtype=tags
+set TagFolder=Rel_1_10_03
+set TagPath=svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/rel_1_10_03
+set trunkPath=svn://svnserver.hq.cas.com.pl/VS/trunk
+
+svn mkdir %TagPath%  -m "created new %TagPath% (in %branchtype% folder)"
+
+svn copy %trunkPath%/PR43-AgileWorkloadTracker %TagPath%/PR43-AgileWorkloadTracker -m "created copy in %TagPath% of the PR43-AgileWorkloadTracker"
+svn copy %trunkPath%/PR39-CommonResources %TagPath%/PR39-CommonResources -m "created copy in %TagPath% of the project PR39-CommonResources"
+svn copy %trunkPath%/ImageLibrary %TagPath%/ImageLibrary -m "created copy in %TagPath% of the project ImageLibrary"
+svn copy %trunkPath%/PR44-SharePoint %TagPath%/PR44-SharePoint -m "created copy in %TagPath% of the project PR44-SharePoint"
+
+pause ....
+
---
+++ VS/trunk/PR43-AgileWorkloadTracker/Scripts/SPDisposeCheck.cmd
@@ -0,0 +1,11 @@
+﻿
+set path="C:\Program Files (x86)\Microsoft\SharePoint Dispose Check"
+set dashbordsPath="C:\vs\Projects\SmartFactory\PR42-SmartFactory\Shepherd\ShepherdDashboards\bin\Release"
+set DataModelPath="C:\vs\Projects\SmartFactory\PR42-SmartFactory\Shepherd\DataModel\bin\Release"
+set WorkflowsPath="C:\vs\Projects\SmartFactory\PR42-SmartFactory\Shepherd\Workflows\bin\Release"
+
+%path%\SPDisposeCheck %dashbordsPath%\CAS.ShepherdDashboards.dll >%dashbordsPath%\CAS.ShepherdDashboards.dclog.txt
+%path%\SPDisposeCheck %DataModelPath%\CAS.ShepherdDataModel.dll >%DataModelPath%\CAS.ShepherdDataModel.dclog.txt
+%path%\SPDisposeCheck %WorkflowsPath%\CAS.SmartFactorySendNotification.dll >%WorkflowsPath%\CAS.SmartFactorySendNotification.dclog.txt
+
+pause...
