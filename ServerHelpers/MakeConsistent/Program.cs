ce4edeb595aa6cfccb0c18eae38c625ce1aff4d3
Mariusz <mpostol@cas.eu>
2014-01-09 08:46:16 +0000

    Add command line Application to the solution - added keywords

--- ServerHelpers/MakeConsistent/Program.cs
+++ ServerHelpers/MakeConsistent/Program.cs
@@ -1,11 +1,11 @@
 ﻿//<summary>
 //  Title   : Name of Application
 //  System  : Microsoft Visual C# .NET 2012
-//  $LastChangedDate:$
-//  $Rev:$
-//  $LastChangedBy:$
-//  $URL:$
-//  $Id:$
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
 //
 //  Copyright (C) 2013, CAS LODZ POLAND.
 //  TEL: +48 (42) 686 25 47
