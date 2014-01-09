7fcbd5833e2a06107e0ca75d796766802c33bf9a
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-01-09 08:32:25 +0000

    Add command line Application to the solution - added project

---
+++ VS/trunk/PR43-AgileWorkloadTracker/ServerHelpers/MakeConsistent/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿//<summary>
+//  Title   : Assembly information
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+      
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AWT Site Cleanup Application")]
+[assembly: AssemblyDescription("CAS Smart Factory Agile Workload Tracker Make Data Content Consistent Application.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("Smart Factory")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2014 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("Smart Factory")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99B74909-7289-4CE0-B9D7-62F168C63190")]
+[assembly: AssemblyVersion("1.10.03.*")]
+[assembly: AssemblyFileVersion("1.10.03")]
+
