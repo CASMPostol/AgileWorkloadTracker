adbe947fcc82e5e871efc2813bc9b69c6d0f42e2
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-03-24 12:44:41 +0000

    AWT website development to 2.00.00 - website modified, new model generated.

--- AgileWorkloadTracker/DataModel/Linq/Scripts/GetEntities.ps1
+++ AgileWorkloadTracker/DataModel/Linq/Scripts/GetEntities.ps1
@@ -1,8 +1,5 @@
 ﻿Write-Host "This file creates all the entities classes for the selected site"
 $env:path += "; C:\Program Files\Common Files\Microsoft Shared\Web server extensions\14\Bin"
 Get-Location | Write-host
-SPMetal.exe /web:http://casmp/sites/awt /language:csharp /namespace:CAS.AgileWorkloadTracker.DataModel.Linq /parameters:Parameters.xml /code:../Entities.cs| write-host
+SPMetal.exe /web:http://casbw/sites/awt6 /language:csharp /namespace:CAS.AgileWorkloadTracker.DataModel.Linq /parameters:Parameters.xml /code:..\Entities.cs| write-host
 write-host "Done . "
-
-
-
