414cc156ab4664ec7aa246b0b6b2543af2c865f1
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-03-30 07:29:30 +0000

    Create database and Linq2SQL entities for archiving AWT 2.00 - linq and lonq2sql - added

---
+++ VS/trunk/PR43-AgileWorkloadTracker/Library/ClientDataManagement/Linq/Scripts/GetEntities.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "This file creates all the entities classes for the selected site"
+$env:path += "; C:\Program Files\Common Files\Microsoft Shared\Web server extensions\14\Bin"
+Get-Location | Write-host
+SPMetal.exe /web:http://casbw/sites/agile /language:csharp /namespace:CAS.AgileWorkloadTracker.Client.DataManagement.Linq /parameters:Parameters.xml /code:..\Entities.cs| write-host
+write-host "Done . "
