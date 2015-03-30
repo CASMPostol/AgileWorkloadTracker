cd313531e924b1a56f0f2d37cadf707dcc567c38
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-03-30 07:29:30 +0000

    Create database and Linq2SQL entities for archiving AWT 2.00 - linq and lonq2sql - added

---
+++ Library/ClientDataManagement/Linq2SQL/Scripts/GetEntities.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "This file creates all the entities classes for the selected SQL database"
+$env:path += "; C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin"
+Get-Location | Write-host
+SQLMetal.exe /server:CASBW /database:AWTARCHIV /namespace:CAS.AgileWorkloadTracker.Client.DataManagement.Linq2SQL /code:..\Entities.cs /language:csharp /context:AWTARCHIV | write-host
+write-host "Done . "
