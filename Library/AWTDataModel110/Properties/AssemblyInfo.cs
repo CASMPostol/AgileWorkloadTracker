625cfe78868579bb06027a5dbfb2005c2ad046dd
Mariusz <mpostol@cas.eu>
2015-04-03 14:02:37 +0000

    Create Wrapper for CAS.AWTDataModel4SOM.dll - updated the version information.

--- Library/AWTDataModel110/Properties/AssemblyInfo.cs
+++ Library/AWTDataModel110/Properties/AssemblyInfo.cs
@@ -5,32 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AWTDataModel110")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AWT DataModel 110")]
+[assembly: AssemblyDescription("Smart Factory Agile Workload Tracker Linq entities and helper functions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AWTDataModel110")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("Smart Factory Agile Workload Tracker")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2015 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("Smart Factory AWT")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c8bc0de0-dbf5-4559-8648-da19c33b880b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
