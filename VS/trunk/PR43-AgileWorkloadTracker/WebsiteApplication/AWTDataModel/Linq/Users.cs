f02a40cc7eef76e1a18c115c465f1c641179f9c5
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-04-03 11:23:33 +0000

    Review data model and fix compile errors for agile 2.00 - compilation errors fixed.

---
+++ VS/trunk/PR43-AgileWorkloadTracker/WebsiteApplication/AWTDataModel/Linq/Users.cs
@@ -0,0 +1,55 @@
+﻿//_______________________________________________________________
+//  Title   : Users
+//  System  : Microsoft VisualStudio 2013 / C#
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (C) 2015, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//_______________________________________________________________
+
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CAS.AgileWorkloadTracker.DataModel.Linq
+{
+  public partial class Users
+  {
+    public static Users FindSPUser(Entities edc, SPUser _user)
+    {
+      if (edc == null)
+        throw new ArgumentNullException("FindForUser requires the edc parameter to be assigned");
+      EntityList<Item> _res = edc.UserInformationList;
+      if (_res == null || _res.Count() == 0)
+        return null;
+      Item _ret = (from idx in _res
+                   let _account = idx.Title
+                   where _account.Contains(_user.Name)
+                   select idx
+                  ).FirstOrDefault();
+      return _ret == null ? null : (Users)_ret;
+    }
+    public IEnumerable<Workload> Workload(Entities edc)
+    {
+      return from idx in edc.Workload
+             where idx.AssignedTo.Id == this.Id
+             select idx
+      ;
+    }
+    public IQueryable<Estimation> Estimation(Entities edc)
+    {
+      return from idx in edc.Estimation
+             where idx.AssignedTo.Id == this.Id
+             select idx
+      ;
+    }
+  }
+}
